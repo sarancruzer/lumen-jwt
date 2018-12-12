760c76a80cc3c0b12ab2ca18a039462d4d7b339c
Saravanan Nandhan <sarancruzer@gmail.com>
2018-12-12 20:04:54 +0530

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    // public function run()
+    // {
+    //     Model::unguard();
+    //     // Register the user seeder
+    //     $this->call(UsersTableSeeder::class);
+    //     Model::reguard();
+    // }
+
+    public function run()
+    {
+        $this->call(UsersTableSeeder::class);
+    }
+}
+
+
